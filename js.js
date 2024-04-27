3eed8dd2629c0be87d9975c686944de163e00eaa
brenor4mos <158362336+brenor4mos@users.noreply.github.com>
2024-04-27 16:31:58 -0300

    teste

--- js.js
+++ js.js
@@ -1,11 +1,33 @@
+let portugues = document.querySelector('#portugues')
+portugues.addEventListener('mouseenter', () => {
+    let h1 = document.querySelector('#portugues > h1')
+    h1.classList.add('cor')
+})
+portugues.addEventListener('mouseleave', () => {
+    let h1 = document.querySelector('#portugues > h1')
+    h1.classList.remove('cor')
+})
 
+let matematica = document.querySelector('#matematica')
+matematica.addEventListener('mouseenter', () => {
+    if(matematica.style.color == '') {
+        matematica.style.color = 'red'
+    }
+})
+matematica.addEventListener('mouseleave', () => {
+    if (matematica.style.color == 'red') {
+        matematica.style.color = ''
+    }
+})
 
-document.querySelector('#burger').addEventListener('click', () => {
-    let menu = document.querySelector('#menu');
-
-    if (menu.style.display == 'none') {
-        menu.style.display = 'flex'
-    } else {
-        menu.style.display = 'none'
+let geografia = document.querySelector('#geografia')
+geografia.addEventListener('mouseenter', () => {
+    if(geografia.style.color == '') {
+        geografia.style.color = 'orange'
+    }
+})
+geografia.addEventListener('mouseleave', () => {
+    if (geografia.style.color == 'orange') {
+        geografia.style.color = ''
     }
 })
