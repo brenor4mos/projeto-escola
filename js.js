72209d57f34eb9604d18889ac2270c7aef394782
brenor4mos <158362336+brenor4mos@users.noreply.github.com>
2024-04-28 16:34:59 -0300

    Versão 2.0

--- js.js
+++ js.js
@@ -3,7 +3,6 @@ let portugues = document.querySelector('#portugues')
 portugues.addEventListener('mouseenter', () => {
     let h1 = document.querySelector('#portugues > h1')
     h1.style.color = 'green'
-
 })
 portugues.addEventListener('mouseleave', () => {
     let h1 = document.querySelector('#portugues > h1')
@@ -30,14 +29,14 @@ geografia.addEventListener('mouseleave', () => {
     h1.style.color = ''
 })
 
-let historia = document.querySelector('#historia')
-historia.addEventListener('mouseenter', () => {
-    let h1 = document.querySelector('#historia > h1')
+let lideranca = document.querySelector('#lideranca')
+lideranca.addEventListener('mouseenter', () => {
+    let h1 = document.querySelector('#lideranca > h1')
     h1.style.color = 'yellow'
     // h1.style.textShadow = '0px 0px 3px black'
 })
-historia.addEventListener('mouseleave', () => {
-    let h1 = document.querySelector('#historia > h1')
+lideranca.addEventListener('mouseleave', () => {
+    let h1 = document.querySelector('#lideranca > h1')
     h1.style.color = ''
     h1.style.textShadow = ''
 })
@@ -49,6 +48,46 @@ fisica.addEventListener('mouseenter', () => {
 })
 fisica.addEventListener('mouseleave', () => {
     let h1 = document.querySelector('#fisica > h1')
+    h1.style.color = ''
+})
+
+let filosofia = document.querySelector('#filosofia')
+filosofia.addEventListener('mouseenter', () => {
+    let h1 = document.querySelector('#filosofia > h1')
+    h1.style.color = 'blue'
+})
+filosofia.addEventListener('mouseleave', () => {
+    let h1 = document.querySelector('#filosofia > h1')
+    h1.style.color = ''
+})
+
+let oratoria = document.querySelector('#oratoria')
+oratoria.addEventListener('mouseenter', () => {
+    let h1 = document.querySelector('#oratoria > h1')
+    h1.style.color = 'purple'
+})
+oratoria.addEventListener('mouseleave', () => {
+    let h1 = document.querySelector('#oratoria > h1')
+    h1.style.color = ''
+})
+
+let educacao_financeira = document.querySelector('#educacao_financeira')
+educacao_financeira.addEventListener('mouseenter', () => {
+    let h1 = document.querySelector('#educacao_financeira > h1')
+    h1.style.color = 'pink'
+})
+educacao_financeira.addEventListener('mouseleave', () => {
+    let h1 = document.querySelector('#educacao_financeira > h1')
+    h1.style.color = ''
+})
+
+let projeto_de_vida = document.querySelector('#projeto_de_vida')
+projeto_de_vida.addEventListener('mouseenter', () => {
+    let h1 = document.querySelector('#projeto_de_vida > h1')
+    h1.style.color = 'brown'
+})
+projeto_de_vida.addEventListener('mouseleave', () => {
+    let h1 = document.querySelector('#projeto_de_vida > h1')
     h1.style.color = ''
 })
 
@@ -68,15 +107,24 @@ function mostrarConteudo(id) {
         botaoVoltar.style.display = 'none'
     })
 
+
     if (id == portugues) {
         iframe.setAttribute('src', 'repositorio/materias_2B/lingua-portuguesa.html')
     } else if (id == matematica){
-        iframe.setAttribute('src', '')
+        iframe.setAttribute('src', 'repositorio/materias_2B/matematica.html')
     } else if (id == geografia) {
-        iframe.setAttribute('src', '')
-    } else if (id == historia) {
-        iframe.setAttribute('src', '')
+        iframe.setAttribute('src', 'repositorio/materias_2B/geografia.html')
+    } else if (id == lideranca) {
+        iframe.setAttribute('src', 'repositorio/materias_2B/lideranca.html')
     } else if (id ==fisica) {
-        iframe.setAttribute('src', '')
+        iframe.setAttribute('src', 'repositorio/materias_2B/fisica.html')
+    } else if (id == filosofia) {
+        iframe.setAttribute('src', 'repositorio/materias_2B/filosofia.html')
+    } else if (id == oratoria) {
+        iframe.setAttribute('src', 'repositorio/materias_2B/oratoria.html')
+    } else if (id == educacao_financeira) {
+        iframe.setAttribute('src', 'repositorio/materias_2B/educacao_financeira.html')
+    } else if (id == projeto_de_vida) {
+        iframe.setAttribute('src', 'repositorio/materias_2B/projeto_de_vida.html')
     }
 }
