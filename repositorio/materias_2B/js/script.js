053e8d072e694b654f77be464d4739df4c266ca2
brenor4mos <158362336+brenor4mos@users.noreply.github.com>
2024-06-03 18:24:38 -0300

    Botão para fim da página

--- repositorio/materias_2B/js/script.js
+++ repositorio/materias_2B/js/script.js
@@ -1,9 +1,13 @@
-const botao = document.querySelector('.botao')
 
 
+const body = document.querySelector('body')
+
+const botao = document.createElement('button')
+botao.setAttribute('class', 'botao')
+body.insertAdjacentElement('beforeend', botao)
 
 
 
 botao.addEventListener('click', () => {
-    window.scrollBy(0, 500000000)
+    window.scrollTo(0, document.body.scrollHeight);
 })
